3ea7ae5b1eb7a35dd1e1a0fc4a516f0561b10dd5
Tobias Brunner <tobias@tobru.ch>
2017-07-31 23:55:39 +0200

    configure app on start

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,27 @@ FROM wallabag/wallabag
 MAINTAINER Tobias Brunner <tobias@tobru.ch>
 
 COPY root /
-RUN chgrp -R 0 \
+WORKDIR /var/www/wallabag
+RUN mkdir \
+      /var/www/wallabag/data/assets \
+    && \
+    chgrp -R 0 \
       /var/www/wallabag \
       /etc/s6 \
       /var/run \
       /var/log \
       /var/lib/nginx \
       /var/tmp/nginx \
-      && \
+    && \
     chmod -R g+rwX \
       /var/www/wallabag \
       /etc/s6 \
       /var/run \
       /var/log \
       /var/lib/nginx \
-      /var/tmp/nginx
+      /var/tmp/nginx \
+    && \
+    rm -rf /var/www/wallabag/var/cache \
+    && \
+    SYMFONY_ENV=prod composer install --no-dev -o --prefer-dist
+
--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-provisioner () {
-    echo "Starting provisioner..."
-    mkdir -p /var/www/wallabag/app
-    mkdir -p /var/www/wallabag/app/config
-    mkdir -p /var/www/wallabag/data
-    mkdir -p /var/www/wallabag/data/assets
-    mkdir -p /var/www/wallabag/data/db
-    echo "Provisioner finished."
+set -x
+
+configure () {
+  sed -i 's/pdo_sqlite/pdo_mysql/' /var/www/wallabag/app/config/parameters.yml
+  sed -i 's@database_host: 127.0.0.1@database_host: '"$SYMFONY__DATABASE_HOST"'@' /var/www/wallabag/app/config/parameters.yml
+  sed -i 's@database_port: null@database_port: 3306@' /var/www/wallabag/app/config/parameters.yml
+  sed -i 's@database_name: symfony@database_name: '"$SYMFONY__DATABASE_NAME"'@' /var/www/wallabag/app/config/parameters.yml
+  sed -i 's@database_user: root@database_user: '"$SYMFONY__DATABASE_USER"'@' /var/www/wallabag/app/config/parameters.yml
+  sed -i 's@database_password: null@database_password: '"$SYMFONY__DATABASE_PASSWORD"'@' /var/www/wallabag/app/config/parameters.yml
 }
 
-if [ "$1" = "wallabag" ];then
-    provisioner
-    exec s6-svscan /etc/s6/
+if [ "$1" = "wallabag" ]; then
+  configure
+  exec s6-svscan /etc/s6/
 fi
 
 exec "$@"
