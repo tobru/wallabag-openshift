e84940f3160cbae46904eccec64a14fed5d551d6
Tobias Brunner <tobias@tobru.ch>
2017-08-01 12:37:32 +0200

    more verbose and config dir bootstrapping

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ MAINTAINER Tobias Brunner <tobias@tobru.ch>
 WORKDIR /var/www/wallabag
 RUN rm -rf /var/www/wallabag/var/cache/prod && \
     SYMFONY_ENV=prod composer install --no-dev -o --prefer-dist && \
-    rm -rf /var/www/wallabag/var/cache/prod && \
     echo "Fixing directory permissions" && \
     chgrp -R 0 \
       /var/www/wallabag/app \
@@ -23,7 +22,9 @@ RUN rm -rf /var/www/wallabag/var/cache/prod && \
       /var/log \
       /var/lib/nginx \
       /var/tmp/nginx && \
-    echo "Finished fixing directory permissions"
+    echo "Finished fixing directory permissions" && \
+    # saving config for restore f.e. on an OpenShift PVC mount
+    tar czf /tmp/wallabag-config.tgz /var/www/wallabag/app/config
 
 COPY root /
 
--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -2,6 +2,12 @@
 
 configure () {
 
+  if [ ! -f /var/www/wallabag/app/config/parameters.yml ]; then
+    echo "---> bootstrapping config directory"
+    tar xzf /tmp/wallabag-config.tgz -C / --no-overwrite-dir
+  fi
+
+  echo "---> editing parameters.yml"
   sed -i 's/pdo_sqlite/pdo_mysql/' /var/www/wallabag/app/config/parameters.yml
   sed -i 's@database_host: 127.0.0.1@database_host: '"$SYMFONY__DATABASE_HOST"'@' /var/www/wallabag/app/config/parameters.yml
   sed -i 's@database_port: null@database_port: 3306@' /var/www/wallabag/app/config/parameters.yml
@@ -10,14 +16,19 @@ configure () {
   sed -i 's@database_password: null@database_password: '"$SYMFONY__DATABASE_PASSWORD"'@' /var/www/wallabag/app/config/parameters.yml
   sed -i 's@redis_host: localhost@redis_host: redis@' /var/www/wallabag/app/config/parameters.yml
 
+  echo "---> clear application cache"
   php bin/console cache:clear -e=prod
+  echo "---> clear application cache - done"
 
+  # Automatic Wallabag installation - if not yet done
   if [ ! -f /var/www/wallabag/app/config/docker.done ]; then
+    echo "---> starting automatic wallabag installation"
     php bin/console wallabag:install --env=prod -n
     exit_status=$?
     if [ $exit_status -eq 1 ]; then
-      echo "automatic installation failed"
+      echo "---> automatic installation failed"
     else
+      echo "---> automatic installation finished"
       echo $(date) > /var/www/wallabag/app/config/docker.done
     fi
   fi
